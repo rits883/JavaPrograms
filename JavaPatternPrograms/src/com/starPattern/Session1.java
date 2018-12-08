61ad2ffb3c74bbf6c49e8204e5227beacad5e687
REETESH KUMAR CHOUBEY <44515254+rits883@users.noreply.github.com>
2018-12-08 21:40:11 +0530

    First Program

---
+++ JavaPatternPrograms/src/com/starPattern/Session1.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.starPattern;
+
+/**
+ *@author REETESH KUMAR CHOUBEY
+ *08-Dec-2018
+  JavaPatternPrograms
+ */
+public class Session1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+     m1();		
+
+	}
+
+	/*
+	 *     ***** 
+	 *     *****
+	 *     *****
+	 *     *****
+	 *     *****
+	 * 
+	 * */
+	public static void m1() {
+		
+	}
+	
+	
+	
+	
+	
+}
