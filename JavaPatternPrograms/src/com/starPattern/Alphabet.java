7381b69c673c8a940582f9d01be1da702ecad801
REETESH KUMAR CHOUBEY <44515254+rits883@users.noreply.github.com>
2018-12-15 17:35:54 +0530

    new changes

--- JavaPatternPrograms/src/com/starPattern/Alphabet.java
+++ JavaPatternPrograms/src/com/starPattern/Alphabet.java
@@ -14,7 +14,7 @@ public class Alphabet {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		m2();
+		m13();
 
 	}
 	
@@ -55,6 +55,248 @@ public class Alphabet {
 	}
 	}
 	
+	/**
+	 * EEEEE
+	 * DDDDD
+	 * CCCCC
+	 * BBBBB
+	 * AAAAA
+	 * **/
+	public static void m3() {
+		char ch ='E';
+		for(int i =1 ;i<=5;i++) {
+			for(int j=5;j>=1;j--) {
+				System.out.print(ch);
+			}
+			ch--;
+			System.out.println();
+		}
+	} 
 	
+/**
+ * EDCBA
+ * EDCBA
+ * EDCBA
+ * EDCBA
+ * EDCBA
+ * */
+	
+	public static void m4() {
+	
+		for(int i=1;i<=5;i++) {
+			char ch ='E';
+			for(int j=5;j>=1;j--) {
+				System.out.print(ch);
+				ch--;
+			}
+			System.out.println();
+		}
+	}
 
+
+
+/**
+ * A
+ * AB
+ * ABC
+ * ABCD
+ * ABCDE
+ * **/
+
+public static void m5() {
+for(int i =1;i<=5;i++) {
+	char ch ='A';
+	for(int j =1;j<=i;j++) {
+		System.out.print(ch);
+		ch++;
+	}
+	System.out.println();
+}	
 }
+
+/**
+ *     A
+ *    AB
+ *   ABC
+ *  ABCD  
+ * ABCDE
+ * **/
+
+public static void m6() {
+	for(int i=1;i<=5;i++) {
+		
+		for(int j=1;j<=(5-i);j++) {
+		System.out.print(" ");	
+		}
+		char ch ='A';
+		for(int k =1;k<=i;k++) {
+			System.out.print(ch);
+			ch++;
+		}
+		System.out.println();
+	}
+}
+
+/**
+ * A
+ * BB
+ * CCC
+ * DDDD
+ * EEEEE
+ * */
+
+public static void m7() {
+	char ch ='A';
+	for(int i=1;i<=5;i++) {
+		for(int j =1;j<=i;j++)
+		{
+			System.out.print(ch);
+		}
+		ch++;
+		System.out.println();
+	}
+}
+
+/***
+ * ABCDE
+ * ABCE
+ * ABC
+ * AB
+ * A
+ * 
+ * **/
+
+
+public static void m8() {
+	for(int i =1;i<=5;i++) {
+		char ch ='A';
+		for(int j=5;j>=i;j--) {
+			System.out.print(ch);
+			ch++;
+		}
+		System.out.println();
+	}
+}
+
+
+/***
+ * ABCDE
+ *  ABCD
+ *   ABC
+ *    AB
+ *     A
+ * **/
+
+public static void m9() {
+	for(int i =1;i<=5;i++) {
+		
+	 for(int j =1;j<=i;j++) {
+		 System.out.print(" ");
+	 }
+	 char ch ='A';
+	 for(int k=0;k<=(5-i);k++) {
+	 System.out.print(ch);
+	 ch++;
+	 }
+	 System.out.println();
+	}
+}
+
+/**
+ * A
+ * AB
+ * ABC
+ * ABCD
+ * ABCDE
+ * ABCD
+ * ABC
+ * AB
+ * A
+ * **/
+
+public static void m10() {
+	int size =4;
+	for(int i =size;i>=-size;i--) {
+		char ch ='A';
+		for(int j =size;j>=Math.abs(i);j-- ) {
+			System.out.print(ch);
+			ch++;
+		}
+		System.out.println();
+	}
+}
+
+
+/**
+ *     A
+ *    AB
+ *   ABC 
+ *  ABCD
+ * ABCDE
+ *  ABCD
+ *   ABC
+ *    AB
+ *     A
+ * **/
+
+public static void m11() {
+	int size =4;
+	for(int i=size;i>=-size;i--) {
+		
+		for(int j=1;j<=Math.abs(i);j++) {
+			  System.out.print(" ");
+		  }
+		char ch = 'A';
+		for(int k =size;k>=Math.abs(i) ;k--) {
+			System.out.print(ch);
+			ch++;
+		}
+		   System.out.println();
+	}
+}
+
+/***
+ *   A
+ *  ABC
+ * ABCDE
+ *ABCDEFG 
+ * */
+
+public static void m12() {
+	int m =1;
+	for(int i =1;i<=4;i++) {
+		for(int j =1;j<=(4-i);j++) {
+			System.out.print(" ");
+		}
+		char ch ='A';
+		for(int k =1;k<=m;k++) {
+			System.out.print(ch);
+			ch++;
+		}
+		m+=2;
+		System.out.println();
+	}
+}
+
+/**
+ *      A
+ *     A B
+ *    A B C
+ *   A B C D
+ * **/
+
+public  static void  m13() {
+	for(int i=1;i<=4;i++) {
+		for(int j =1;j<=(4-i);j++) {
+			System.out.print(" ");
+		}
+		char ch ='A';
+		for(int j=1;j<=i;j++) {
+			System.out.print(ch+" ");
+			ch++;
+		}
+		System.out.println();
+	}
+}
+}
+
